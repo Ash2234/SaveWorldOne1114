d220b41b7acf2d7d2cc55cb740921f6b0ffba8d5
Ash 2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-07 08:58:01 +0000

    Nth

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+  numbers=[1, 2, 3, 4, 5]
+  with open('text.txt', 'r') as file:
+    intro=file.read()
+  data=None
+  if request.method == 'POST':
+    data=request.form['data']
+    data=data.upper() #whatever you want to do here
+    with open('data.txt', 'a') as file:
+      file.write(data + "\n")
+    return render_template('index.html', data=data, intro=intro, nums=numbers)
 
 
 app.run(host='0.0.0.0', port=81)
