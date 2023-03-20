215e6ca480091347e756d7731ed960dc5e88a527
Ash 2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-20 13:46:05 +0000

    Nth the code jus doesn’t want to fking work

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
-data=[Math, Gp, Physics, Chemistry, Biology, Econs]
-
 def index():
   subject=None
   contact=None
   if request.method=='POST':
     subject=request.form["subject"]
     contact=request.form["contact"]
-    for x in data:
-      if x==subject:
-        x.append(contact)
-        break
   return render_template('index.html', subject=subject, contact=contact)
 
 
