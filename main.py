9dace31c1f72478af358eeacb35d5fa0fc920d3e
Ash 2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-20 13:37:15 +0000

    Things

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from flask import Flask
-import csv
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
+
+Math=[]
+Gp=[]
+Physics=[]
+Chemistry=[]
+Biology=[]
+Econs=[]
+data=[Math, Gp, Physics, Chemistry, Biology, Econs]
+
 def index():
-  numbers=[1, 2, 3, 4, 5]
-  with open('text.txt', 'r') as file:
-    intro=file.read()
-    
-  data=None
-  if request.method == 'POST':
-    data=request.form['data']
-    data=data.upper() #whatever you want to do here
-    with open('data.txt', 'a') as file:
-      file.write(data + "\n")
-    return render_template('index.html', data=data, intro=intro, nums=numbers)
+  subject=None
+  if request.method=='POST':
+    subject=request.form["subject"]
+    contact=request.form["contact"]
+  return render_template('index.html', subject=subject, contact=contact)
+
+
 
 
 app.run(host='0.0.0.0', port=81)
