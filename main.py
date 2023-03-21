99f9a84567f0728f5f55640e0e8e719c6b28a3e9
Ash 2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-21 05:25:58 +0000

    Pp time

--- main.py
+++ main.py
@@ -7,12 +7,56 @@ app = Flask(__name__)
 def index():
   subject=None
   contact=None
+  pp=""
   if request.method=='POST':
     subject=request.form["subject"]
     contact=request.form["contact"]
-  return render_template('index.html', subject=subject, contact=contact)
+  if subject=="Math":
+    with open("Math.txt", "r") as file:
+      pp=file.read()
+    with open("Math.txt", "a") as file:
+      file.write(contact+"\n")
+
+      
+  if subject=="Biology":
+    with open("Biology.txt", "r") as file:
+      pp=file.read()
+    with open("Biology.txt", "a") as file:
+      file.write(contact+"\n")
+
+      
+  if subject=="Chemistry":
+    with open("Chemistry".txt, "r") as file:
+      pp=file.read()
+    with open("Chemistry.txt", "a") as file:
+        file.write(contact+"\n")
+
+      
+  if subject=="Econs":
+    with open("Econs.txt", "r") as file:
+      pp=file.read()
+    with open("Econs.txt", "a") as file:
+        file.write(contact+"\n")
+
+      
+  if subject=="Gp":
+    with open("Gp.txt", "r") as file:
+      pp=file.read()
+    with open("Gp.txt", "a") as file:
+        file.write(contact+"\n")
+
+      
+  if subject=="Physics":
+    with open("Physics.txt", "r") as file:
+      pp=file.read()
+    with open("Physics.txt", "a") as file:
+        file.write(contact+"\n")
+
+      
+    
+  return render_template('index.html', subject=subject, contact=contact, pp=pp)
 
 
 
 
-app.run(host='0.0.0.0', port=81)
+app.run(debug=True, host='0.0.0.0', port=81)
